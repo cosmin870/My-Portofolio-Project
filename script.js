c64d0e17e0d6cf8091a94d6d467d8d2a21c26c82
cosmin870 <cosmin_ilie99@yahoo.com>
2022-01-09 23:17:52 +0200

    making about me page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const hamburger = document.getElementById("hamburger");
+const navUl = document.getElementById("nav_ul");
 
+hamburger.addEventListener("click", () => {
+  navUl.classList.toggle("show");
+});
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("clicked");
+});
